60d96615749694513848e177c5ecd591ea4566fe
Karanveer-Mul <karanveer7171@gmail.com>
2020-08-23 04:02:55 +0530

    card and modal onClick

--- src/components/show/cardHolder.js
+++ src/components/show/cardHolder.js
@@ -14,31 +14,20 @@ const CardHolder = (props) => {
 
   return (
     <>
-      <Card
-        className="text-white border-0 col-xs-12 col-sm-12 col-md-12 col-lg-2 rounded-0"
-        style={{ width: "8rem" }}
-      >
-        <Card className="showImage">
-          <Card.Img variant="top img-fluid" src={image_url} />
+      <Card className="text-white mt-2 border-0 col-xs-8 col-sm-8 col-md-4 col-lg-2 showImage rounded-0"> 
+          <Card.Img variant="top img-fluid showImage" src={image_url} />
           <Card.ImgOverlay
-            className="d-flex flex-column show"
+            className="overlay-image"
             onClick={() => setSummaryShown(true)}
           >
+          <div className="badges">
             <Badge variant="primary">{type}</Badge>
             <Badge variant="warning">{score}</Badge>
+          </div>
             <Card.Text className="mt-auto  showTitle">
-              <h6>{title}</h6>
+              {title}
             </Card.Text>
           </Card.ImgOverlay>
-        </Card>
-        {/*<Card.Footer>
-          <Button
-            variant="dark float-right"
-            onClick={() => setSummaryShown(true)}
-          >
-            ADD
-          </Button>{" "}
-        </Card.Footer>*/}
       </Card>
       <ShowSummary
         show={isSummaryShown}
