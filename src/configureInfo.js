82ca021a5f28212539ded3a0a00fc9f12efce3c2
Karanveer-Mul <karanveer7171@gmail.com>
2020-09-25 08:31:27 +0530

    removed config file

---
+++ src/configureInfo.js
@@ -0,0 +1,8 @@
+const ConfigureInfo = {
+  CLIENT_ID:
+    "715909202549-ofpm58s32hohf2h03bm4tds2icaitvbp.apps.googleusercontent.com",
+
+  API_KEY: "AIzaSyCNb-2Csp836XFgENzb3F8N12FQwjwvT5E",
+};
+
+export default ConfigureInfo;
