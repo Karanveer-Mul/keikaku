e657585bdbdcce291846a01bffb67d005329d419
Karanveer-Mul <karanveer7171@gmail.com>
2020-12-13 02:32:13 +0530

    Restructure of landing page intialised

--- src/components/layout/home.js
+++ src/components/layout/home.js
@@ -4,23 +4,23 @@ import { Row, Container } from "react-bootstrap";
 class Home extends Component {
   render() {
     return (
-      <Container className="m-auto" fluid>
-        <br></br>
+      <Container className="home">
         <Row>
-          <h2 style={{ fontFamily: "Secular One" }}>How to use?</h2>
+          <h2>How to use?</h2>
         </Row>
         <Row>
-          <p style={{ fontFamily: "Secular One", fontSize: "150%" }}>
+          <p>
             To load the list of seasonal anime, click on <b>Season</b> in the
             navbar.
-            <br></br>
+          </p>
+          <p>
             Select any show and click on <b>Set Event</b> and you will be
             redirected to log into your google account.
-            <br></br>
-            Log into the account which you want to use for setting the event.
-            <br></br>
-            The event will be added to your calendar.
           </p>
+          <p>
+            Log into the account which you want to use for setting the event.
+          </p>
+          <p>The event will be added to your calendar.</p>
         </Row>
       </Container>
     );
