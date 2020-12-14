73ed83dc7adf3abda7fb8d997e056d2fd777409e
Karanveer-Mul <karanveer7171@gmail.com>
2020-12-15 02:41:02 +0530

    watch list called for 1 week

---
+++ src/components/list/watchList.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import ConfigureInfo from "../../configureInfo";
+import moment from "moment";
+import { Button } from "react-bootstrap";
+
+const WatchList = () => {
+  const [events, setEvents] = useState(null);
+
+  var gapi = window.gapi;
+  var CLIENT_ID = ConfigureInfo.CLIENT_ID;
+  var API_KEY = ConfigureInfo.API_KEY;
+
+  var DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+
+  var SCOPES = "https://www.googleapis.com/auth/calendar.events";
+
+  const handleClick = () => {
+    const until =
+      moment(new Date()).add(7, "days").format("YYYY-MM-DDTHH:mm:ss") + "Z";
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calender", "v3");
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(async () => {
+          const response = await gapi.client.calendar.events.list({
+            calendarId: "primary",
+            timeMax: until,
+            timeMin: new Date().toISOString(),
+            showDeleted: false,
+            singleEvents: true,
+            maxResults: 30,
+            orderBy: "startTime",
+          });
+          const items = await response.result.items;
+          setEvents(items);
+        });
+    });
+  };
+  return (
+    <div style={{ float: "right" }}>
+      <Button type="primary" onClick={() => handleClick()}>
+        Select Profile
+      </Button>
+    </div>
+  );
+};
+
+export default WatchList;
